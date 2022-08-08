a2d0de3dca7f3c8d0eb6fc0c2989e46bcddb2d49
Cai Gao <cgao@corvusinsurance.com>
2022-08-08 16:44:34 -0400

    Function with default value in arg

--- checkout.ex
+++ checkout.ex
@@ -2,6 +2,8 @@ defmodule Checkout do
   def total_cost(price, tax_rate) do
     price * (tax_rate + 1)
   end
+
+  def total_cost_default(price, quantity \\ 10), do: price * quantity
 end
 
 IO.puts Checkout.total_cost(10,2)
